ea4035ef5acaea8b060de26b3472ab0a500679ab
Prasad Pathare <prasadsachinist10@gmail.com>
2024-10-18 09:18:38 +0000

    completed renderOptions & fetch functions

--- src/main.js
+++ src/main.js
@@ -1,1 +1,54 @@
-console.log("Connected")
+// DOM Selection
+const selectEl = document.querySelector("select");
+const carouselContainerEl = document.querySelector(".carousel-inner");
+
+const BASE_URL = "https://dog.ceo/api/";
+
+// === MARK: Fetch
+// Gets the list of dog breeds from api
+async function getDogsList() {
+  try {
+    const response = await fetch(`${BASE_URL}breeds/list/all`);
+    const data = await response.json();
+    return Object.keys(data.message);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Gets list of 10 images on breed
+async function getDogImages(breed) {
+  try {
+    const res = await fetch(`${BASE_URL}breed/${breed}/images`);
+    const data = await res.json();
+    return data.message.slice(0, 10);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// === MARK: Render
+async function renderOptions() {
+  const breedList = await getDogsList();
+
+  for (breed of breedList) {
+    // ek option element create karo
+    const option = document.createElement("option");
+    option.textContent = breed[0].toUpperCase() + breed.slice(1).toLowerCase
+    // option ke andar breed ka naam likho
+    option.textContent = breed;
+    // select ke andar render karao
+    selectEl.appendChild(option);
+  }
+}
+
+renderOptions();
+
+async function renderCarousel(breed) {
+const images = await getDogImages(breed);
+console.log(images)
+}
+
+selectEl.addEventListener("change", function(e){
+    renderCarousel(e.target.value)
+});
